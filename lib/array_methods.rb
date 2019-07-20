a11043788e0f07d8e82f495adef251cf24cf1155
Renee France <renee5.0@outlook.com>
2019-07-20 16:08:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,7 +8,7 @@ def using_sort(array)
 end
 
 def using_reverse(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+ array = ["wow", "I", "am", "really", "learning", "arrays!"]
  array.reverse
 end
 
